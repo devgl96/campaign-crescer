eb027cedce4a9b1f8c7e994f901c3813faccea2b
devgl96 <georgelucas.dev@gmail.com>
2022-03-02 12:49:22 -0300

    Create a video component

---
+++ src/components/videoDepo/index.tsx
@@ -0,0 +1,7 @@
+export function videoDepo() {
+  return (
+    <video width="326" height="160" controls>
+      <source src="https://www.youtube.com/watch?v=GHTA143_b-s" type="video/mpeg" />
+    </video>
+  )
+}
