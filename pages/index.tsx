b5054744795cde222a476a407c192e26f15be79f
devgl96 <georgelucas.dev@gmail.com>
2022-03-09 11:12:23 -0300

    Change the directory src to main folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,6 +20,7 @@ import {
 } from "react-share";
 
 import styles from '../styles/Home.module.scss';
+import Link from 'next/link';
 
 interface VideoInfo {
   videoId: string;
@@ -71,20 +72,44 @@ export default function Home({data}: HomeProps) {
         </div>
         <TitleSection title={"OBJETIVO"} />
         <div className={styles.goalCampaign}>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae adipisci, at inventore et, vel culpa consectetur illum sequi sit minus exercitationem doloribus nihil placeat repellendus quo aperiam eligendi esse. Adipisci quibusdam recusandae tempore tempora inventore, optio incidunt a. A, cumque non rem mollitia eum ducimus numquam error dignissimos nobis assumenda, est dolorum qui nam adipisci voluptates doloremque veritatis reiciendis praesentium laborum possimus explicabo! Laudantium, ea impedit? Laudantium necessitatibus saepe at dolorem molestias unde tempore, reprehenderit atque praesentium non temporibus iure itaque dolor sapiente maxime in excepturi amet voluptatum quam architecto ab? Obcaecati cumque dicta totam at aspernatur perferendis nemo fuga, fugit est ut necessitatibus doloribus cum perspiciatis rem hic expedita dolorem repellendus et optio magnam tempora quo, illo corrupti dolorum! Quod et, numquam pariatur impedit dignissimos praesentium similique, omnis assumenda asperiores ex quia odio fugiat exercitationem laudantium quae. Ex aliquam consequatur placeat aut eum velit iusto nam at minus nobis.</p>
+          <p>
+            Somos uma escola dedicada e compromissada com a educação de nossas crianças; durante vários anos temos procurado dar o melhor do que está ao nosso alcance, para que os nossos pequenos aprenda a dar os seus primeiros passos na vida através de um ensino sadio no caráter e no convívio com a sociedade; porque sabemos que é nos primeiros anos de vida do indivíduo que é formada a sua personalidade. Porém mesmo sendo uma das escolas de referência no ensino infantil em nossa cidade, temos tido uma grande necessidade para
+            mantermos nossa escola funcionando; a necessidade urgente de uma sede própria.
+          </p>
           <img src="https://source.unsplash.com/random" alt="random image by unsplash" />
         </div>
         <TitleSection title={"HISTÓRIA"} />
         <div className={styles.historySchool}>
           <div className={styles.leftSide}>
             <img src="https://source.unsplash.com/random" alt="random image by unsplash" />
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente ducimus alias, nobis facilis, fuga mollitia perspiciatis, a recusandae sed cumque nam aliquam corporis ex placeat tenetur. Consectetur, laborum at? Corrupti architecto laborum illum aliquam ipsum tempora aperiam rem impedit consequuntur modi quod nesciunt, dignissimos iste aspernatur harum rerum ipsam veniam!</p>
+            <p>
+              A nossa escola nasceu da ideia de mães que queriam um ensino de qualidade e com um valor de mensalidade que fosse accessível. Essas mães em sua grande maioria, pertenciam a Igreja Presbiteriana do Brasil, localizada à rua Castro Alves, nº 73, Camacã - Bahia.<br />
+
+              Assim, no ano de 1989 foi cedida uma sala nas dependências da Igreja Presbiteriana da nossa cidade, local onde teve início às primeiras aulas, apenas com crianças de 03 e 04 anos, com o nome de Escola Presbiteriana de Camacã;  com o passar do tempo e com a adesão de mais crianças a cada ano, foi-se formando mais turmas chegando até a alfabetização.<br />
+
+              Após alguns anos de funcionamento, foi solicitado a desocupação do local, e a escola passou a funcionar em locais alugados, no total de 4 locais, e teve o seu nome alterado, passando a chamar-se Escola de Educação Infantil Crescer.<br />
+            </p>
           </div>
           <div className={styles.rightSide}>
             <img src="https://source.unsplash.com/random" alt="random image by unsplash" />
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis nam consequuntur repellendus reiciendis, commodi consectetur ipsam vitae dolor modi facilis. Quaerat saepe consectetur quam suscipit architecto eos mollitia cupiditate, incidunt numquam necessitatibus odit a neque distinctio doloremque repellat. Reiciendis, repellat. Ratione nostrum rerum atque quasi, culpa cum doloribus distinctio voluptatibus maxime sed vitae doloremque, dolorum saepe praesentium, inventore fugiat facilis dolor neque quas blanditiis! Dignissimos iste officia corporis cupiditate in cum eius aliquid recusandae blanditiis? </p>
+            <p>
+              Com a ajuda de Deus e muita luta, já conseguimos comprar um terreno, mas devido às grandes
+              dificuldades financeiras em que temos vivido não só no nosso país, mas no mundo inteiro,
+              ainda não foi possível construir a nossa escola; mas creio e sei que quando há solidariedade,
+              quando as mãos se juntam muita coisa pode ser feita e é por isso que estamos aqui nos
+              utilizando desse meio de ajuda mútua, para pedir a sua colaboração, a sua contribuição, para
+              que possamos pelo menos iniciar a construção da nossa sede, o local próprio da escola crescer.
+              Por favor não deixe nossa escola fechar as portas; só nós sabemos o quanto vai nos fazer falta
+              se formos forçados a tomar tal atitude, pois, dependemos dessa escola para ajudar no
+              sustento das nossas famílias e temos prazer em transmitir o nosso conhecimento para nossas
+              crianças.<br />
+              Abra o seu coração e faça sua doação, mesmo que seja o valor mínimo estipulado nessa
+              campanha.
+            </p>
           </div>
-          <p className={styles.knowMore}>Conheça mais do nosso trabalho <a href="https://www.instagram.com/eeicrescer/" target="_blank" rel="noreferrer"><img src="/images/instagramIcon.svg" alt="Instagram" /></a></p>
+          <p className={styles.knowMore}>
+            Conheça mais do nosso trabalho <a href="https://www.instagram.com/eeicrescer/" target="_blank" rel="noreferrer"><img src="/images/instagramIcon.svg" alt="Instagram" /></a>
+          </p>
         </div>
         <TitleSection title={"DEPOIMENTOS"} />
         
@@ -116,14 +141,18 @@ export default function Home({data}: HomeProps) {
         <TitleSection title={"NOS AJUDE"} />
         <div className={styles.helpSection}>
           <h3>PIX 000.000.000-00</h3>
-          <button>Doe pela Nossa Vakinha</button>
+          <a href={"http://vaka.me/2710047"} target={"_blank"} rel="noreferrer">
+            <button>
+                Doe pela Nossa Vakinha
+            </button>
+          </a>
         </div>
         <TitleSection title={"COMPARTILHE"} />
         <div className={styles.footer}>
           <p>Divulgue esta campanha nas redes sociais</p>
           <div className={styles.socialMedias}>
             <FacebookShareButton 
-              url={"https://www.instagram.com/"}
+              url={"http://vaka.me/2710047"}
               quote={"Ajude a Escola Crescer! Precisamos de toda a ajuda possível para que a nossa escola não feche as portas."}
               hashtag={"#escolaCrescer, #campanhaEscolaCrescer, #ajudeEscolaCrescer"}
             >
@@ -131,7 +160,7 @@ export default function Home({data}: HomeProps) {
             </FacebookShareButton>
 
             <TwitterShareButton
-              url={"https://www.instagram.com/"}
+              url={"http://vaka.me/2710047"}
               title="Ajude a Escola Crescer! Precisamos de toda a ajuda possível para que a nossa escola não feche as portas."
               hashtags={["escolaCrescer", "campanhaEscolaCrescer", "ajudeEscolaCrescer"]}
             >
@@ -139,7 +168,7 @@ export default function Home({data}: HomeProps) {
             </TwitterShareButton>
 
             <WhatsappShareButton
-              url="https://www.instagram.com/"
+              url="http://vaka.me/2710047"
               title="Ajude a Escola Crescer! Precisamos de toda a ajuda possível para que a nossa escola não feche as portas."
             >
               <img src="/images/whatsappIcon.svg" alt="Whatsapp" />
@@ -147,7 +176,7 @@ export default function Home({data}: HomeProps) {
 
             {/* With Problem */}
             <LinkedinShareButton
-              url="http://google.com/"
+              url="http://vaka.me/2710047"
               title="Ajude a Escola Crescer!"
               summary="Precisamos de toda a ajuda possível para que a nossa escola não feche as portas."
               source="Ajuda voltada para a Escola de Educação Infantil Crescer"
@@ -156,14 +185,14 @@ export default function Home({data}: HomeProps) {
             </LinkedinShareButton>
 
             <TelegramShareButton
-              url="http://google.com/"
+              url="http://vaka.me/2710047"
               title="Ajude a Escola Crescer! Precisamos de toda a ajuda possível para que a nossa escola não feche as portas."
             >
               <TelegramIcon size="40" borderRadius={50} />
             </TelegramShareButton>
 
             <EmailShareButton
-              url="http://google.com/"
+              url="http://vaka.me/2710047"
               subject="Ajude a Escola Crescer!"
               body="Precisamos de toda a ajuda possível para que a nossa escola não feche as portas."
             >
